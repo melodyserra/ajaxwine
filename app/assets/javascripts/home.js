ee9c0d0e0df011635e50a9474532a0ce59363a7c
Melody Serra <melodyserra@gmail.com>
2014-11-18 09:25:25 -0800

    first commit

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  
+  function getWine(){
+    return $.getJSON("http://daretodiscover.net/wine", function(){});
+  }
+
+  // $(".wineSearch").on("click", function(e){
+  //   e.preventDefault();
+
+    $.when(getWine()).done(function(result){
+      console.log(result);
+
+      var compliedTemplate = HandlebarsTemplates['wine']({result: result});
+
+      $(".wineData").append(compliedTemplate);
+      
+    });
+
+  // });
+
+});
